b44e0f520430c055d7edec3b8a93b2eac55b7918
mert.kaygusuz <mert.kaygusuz>
2022-11-12 16:25:20 +0300

    start for .net 7 migration

--- CityLibrary/Program.cs
+++ CityLibrary/Program.cs
@@ -1,26 +1,105 @@
-using Microsoft.AspNetCore.Hosting;
+using System.Runtime.ExceptionServices;
+using System.Text.Json.Serialization;
+using CityLibrary.ServicesExtensions;
+using CityLibraryApi.MapperConfigurations;
+using CityLibraryInfrastructure.AppSettings;
+using CityLibraryInfrastructure.ExceptionHandling;
+using CityLibraryInfrastructure.TokenExtensions;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
-namespace CityLibrary
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration;
+IWebHostEnvironment environment = builder.Environment;
+
+builder.Services.Configure<AppSetting>(configuration);
+var appSetting = configuration.Get<AppSetting>();
+
+builder.Services.AddMvcCore().AddFluentValidation(fv =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    fv.RegisterValidatorsFromAssemblyContaining<MapsterMapping>();
+    fv.ValidatorOptions.CascadeMode = CascadeMode.Continue;
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddRangeCustomServices(appSetting);
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.WriteIndented = true;
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CityLibrary", Version = "v1" });
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter JWT Bearer token **_only_**",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
                 {
-                    webBuilder.UseStartup<Startup>();
+                    {securityScheme, Array.Empty<string>()}
                 });
-    }
+});
+
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CityLibrary v1"));
 }
+
+app.UseStaticHttpContext();
+
+app.UseCors(opts =>
+        //TODO: content disposition explanation
+        opts.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().WithExposedHeaders("Content-Disposition")
+);
+
+app.UseExceptionHandler(a => a.Run(async context =>
+{
+    var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+    var exception = exceptionHandlerPathFeature.Error;
+
+    if (exception is CustomHttpException exception1)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)exception1.HttpStatusCode;
+        await context.Response.WriteAsync(exception.Message);
+    }
+    else
+    {
+        ExceptionDispatchInfo.Capture(exception).Throw();
+    }
+}));
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
+
